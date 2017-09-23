3e6f26bcf0f21165c69eb317a3af5057fe8fd491
Yaroslav Bulatov <yaroslavvb@gmail.com>
2017-09-23 14:45:21 -0700

    improve benchmark

--- gpu_svd_bench.py
+++ gpu_svd_bench.py
@@ -1,29 +1,32 @@
 '''
-Created on Sep 21, 2017
+Running on I7/GTX 1080
 
-modified from voldemaro
-
-On I7/GTX 1080
-
-MKL detected
 MKL version b'Intel(R) Math Kernel Library Version 2017.0.3 Product Build 20170413 for Intel(R) 64 architecture applications'
 TF version:  b'v1.3.0-rc1-2487-g088cdea'
 TF url:  https://github.com/tensorflow/tensorflow/commit/088cdea
-pre-warming: 9.510
-TF GPU 5.553296
-TF CPU 6.508888
-numpy default: 0.302414
-numpy gesdd: 0.251199
-numpy gesvd 1.485335
-
+Timing in ms for 1534 x 1534 SVD of type <class 'numpy.float32'>
+numpy default        min:   243.52, median:   246.42, mean:   259.49
+numpy gesvd          min:  1294.13, median:  1296.60, mean:  1298.93
+numpy gesdd          min:   242.36, median:   242.80, mean:   245.32
+TF CPU               min:   950.77, median:  1080.64, mean:  1050.79
+TF GPU               min:  5483.26, median:  5520.19, mean:  5571.15
 '''
+
+from scipy import linalg  # for svd
 import numpy as np
 import os
-os.environ["TF_CPP_MIN_LOG_LEVEL"]="2"
+import sys
+import time
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"]="2"  # nospam
+
 import tensorflow as tf
-import time;
-import numpy.linalg as NLA;
-from scipy import linalg  # for svd
+import gc; gc.disable()
+
+NUM_RUNS = 11
+dtype = np.float32
+N=1534;
+
 
 def get_tensorflow_version_url():
     import tensorflow as tf
@@ -47,9 +50,46 @@ def get_mkl_version():
     mkl.MKL_Get_Version_String(ver.ctypes.data_as(ctypes.c_char_p), 198)
     return ver[ver != 0].tostring()
 
+timeline_counter = 0
+run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+def traced_run(fetches):
+    """Runs fetches, dumps timeline files in current directory."""
+    from tensorflow.python.client import timeline
+
+    global timeline_counter
+    run_metadata = tf.RunMetadata()
+
+    results = sess.run(fetches,
+                       options=run_options,
+                       run_metadata=run_metadata);
+    tl = timeline.Timeline(step_stats=run_metadata.step_stats)
+    ctf = tl.generate_chrome_trace_format(show_memory=True,
+                                          show_dataflow=False)
+    open("timeline_%d.json"%(timeline_counter,), "w").write(ctf)
+    open("stepstats_%d.pbtxt"%(timeline_counter,), "w").write(str(
+        run_metadata.step_stats))
+    timeline_counter+=1
+    return results
+
+def benchmark(message, func):
+    time_list = []
+    for i in range(NUM_RUNS):
+        start_time = time.perf_counter()
+        func()
+        time_list.append(time.perf_counter()-start_time)
+
+    time_list = 1000*np.array(time_list)  # get seconds, convert to ms
+    if len(time_list)>0:
+        min = np.min(time_list)
+        median = np.median(time_list)
+        formatted = ["%.2f"%(d,) for d in time_list[:10]]
+        result = "min: %8.2f, median: %8.2f, mean: %8.2f"%(min, median, np.mean(time_list))
+    else:
+        result = "empty"
+    print("%-20s %s"%(message, result))
+    
 
 if np.__config__.get_info("lapack_mkl_info"):
-    print("MKL detected")
     print("MKL version", get_mkl_version())
 else:
     print("no MKL")
@@ -57,53 +97,32 @@ else:
 print("TF version: ", tf.__git_version__)
 print("TF url: ", get_tensorflow_version_url())
 
-N=1534;
 
-dtype = np.float32
 svd_array = np.random.random_sample((N,N)).astype(dtype);
 
 with tf.device("/gpu:0"):
     init_holder_gpu = tf.placeholder(dtype, shape=(N,N))
-    specVarGPU = tf.Variable(init_holder_gpu, dtype=dtype);
+    specVarGPU = tf.random_uniform((N,N), dtype=dtype)
     [D2_gpu, E1_gpu,  E2_gpu] = tf.svd(specVarGPU);
 with tf.device("/cpu:0"):
     init_holder_cpu = tf.placeholder(dtype, shape=(N,N))
-    specVarCPU = tf.Variable(init_holder_cpu, dtype=dtype);
+    specVarCPU = tf.random_uniform((N,N), dtype=dtype)
     [D2_cpu, E1_cpu,  E2_cpu] = tf.svd(specVarCPU);
 
+print("Timing in ms for %d x %d SVD of type %s"%(N, N, dtype))
+def func(): linalg.svd(svd_array)
+benchmark("numpy default", func)
 
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=False))
+def func(): linalg.svd(svd_array, lapack_driver='gesvd');
+benchmark("numpy gesvd", func)
 
-# Initialize all tensorflow variables
-start = time.time();
-sess.run(specVarGPU.initializer, feed_dict={init_holder_gpu: svd_array});
-sess.run(specVarCPU.initializer, feed_dict={init_holder_cpu: svd_array});
-    
-[d, e1, e2]  = sess.run([D2_gpu.op, E1_gpu.op,  E2_gpu.op]);
-[d, e1, e2]  = sess.run([D2_cpu.op, E1_cpu.op,  E2_cpu.op]);
-u, s, v = linalg.svd(svd_array);
-u, s, v = linalg.svd(svd_array, lapack_driver='gesdd');
-u, s, v = linalg.svd(svd_array, lapack_driver='gesvd');
-print('pre-warming: %.3f'%(time.time()-start))
+def func(): linalg.svd(svd_array, lapack_driver='gesdd');
+benchmark("numpy gesdd", func)
 
-start_time = time.time();
-[d, e1, e2]  = sess.run([D2_gpu.op, E1_gpu.op,  E2_gpu.op]);
-print("TF GPU %.6f"%(time.time() - start_time))
+sess = tf.Session()
 
-[d, e1, e2]  = sess.run([D2_cpu.op, E1_cpu.op,  E2_cpu.op]);
-print("TF CPU %.6f" %(time.time() - start_time))
+def func(): sess.run([D2_cpu.op, E1_cpu.op,  E2_cpu.op])
+benchmark("TF CPU", func)
 
-# Defaut numpy (gesdd)
-start = time.time();
-u, s, v = linalg.svd(svd_array);
-print('numpy default: %.6f'%(time.time() - start))
-
-start = time.time();
-u, s, v = linalg.svd(svd_array);
-print('numpy gesdd: %.6f'%(time.time() - start))
-
-# Numpy gesvd
-start = time.time();
-u, s, v = linalg.svd(svd_array, lapack_driver='gesvd');
-print('numpy gesvd %.6f'%(time.time() - start))
-
+def func(): sess.run([D2_gpu.op, E1_gpu.op,  E2_gpu.op])
+benchmark("TF GPU", func)
